f3016be3d4115bf063a1887b846ac9879a1b0e11
daslu <daniel.slutsky@gmail.com>
2026-04-03 13:09:39 +0300

    index page fixes

--- notebooks/index.clj
+++ notebooks/index.clj
@@ -9,19 +9,18 @@
    [clojure.string :as str]
    [scicloj.kindly.v4.kind :as kind]))
 
-;; Copy readme_files to notebooks path so Clay can find them for the book
-^{:kindly/hide-code true}
+^:kind/hidden
 (let [src (io/file "readme_files")
       dst (io/file "notebooks" "readme_files")]
+  ;; Copy readme_files to notebooks path so Clay can find them for the book
   (when (.exists src)
     (.mkdirs dst)
     (doseq [f (.listFiles src)]
       (io/copy f (io/file dst (.getName f))))))
 
-^{:kindly/hide-code true
-  :kind/md true}
+^:kind/md
 (->> "README.md"
      slurp
      str/split-lines
-     (drop 2)
+     (drop 3)
      (str/join "\n"))
